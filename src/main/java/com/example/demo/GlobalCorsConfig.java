1a4627e2e3f7dc39c8bb51a419b63f34cc619622
sanazmi <sanazmi1980@gmail.com>
2025-01-24 18:09:43 +0530

    Updated Java files and static resources

--- src/main/java/com/example/demo/GlobalCorsConfig.java
+++ src/main/java/com/example/demo/GlobalCorsConfig.java
@@ -1,27 +1,5 @@
 package com.example.demo;
 
-// package com.example.password_generator;
-
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-
-// @Configuration
-// public class WebConfig implements WebMvcConfigurer {
-
-//     @Override
-//     public void addCorsMappings(CorsRegistry registry) {
-//         registry.addMapping("/**")
-//                 .allowedOrigins("http://localhost:8081") // Add the origin of your frontend
-//                 .allowedMethods("GET", "POST")
-//                 .allowedHeaders("*");
-//     }
-// }
-
-
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -46,24 +24,22 @@ import org.springframework.web.filter.CorsFilter;
 //     }
 // }
 
-
 @Configuration
 public class GlobalCorsConfig {
 
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:8083");
-        config.addAllowedOrigin("https://sanazmi.github.io");
-        config.addAllowedOrigin("https://github.com/sanazmi/pg-spring");
-        config.addAllowedOrigin("https://pg-spring-production.up.railway.app/");
-        config.setAllowCredentials(true); 
-        config.addAllowedMethod("*"); // Allow all methods
+        config.addAllowedOrigin("http://localhost:8083"); // Localhost
+        config.addAllowedOrigin("https://github.com/sanazmi"); // GitHub Pages
+        config.addAllowedOrigin("http://pg-spring-production.up.railway.app"); // Railway
+
+        config.setAllowCredentials(true); // Allow cookies/authentication
+        config.addAllowedMethod("*"); // Allow all HTTP methods
         config.addAllowedHeader("*"); // Allow all headers
-        config.addExposedHeader("Access-Control-Allow-Origin");
-        config.addExposedHeader("Access-Control-Allow-Credentials");
         config.setMaxAge(3600L); // Cache preflight response for 1 hour
 
+        // Register configuration for all routes
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
